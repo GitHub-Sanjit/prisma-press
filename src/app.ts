aadf5736c98a0ce70a57d3897d078aa83dd974b2
sanjit <sanjit.est14@gmail.com>
2026-06-28 20:11:10 +0600

    Multi Schema prisma file setup

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express, { Application, Request, Response } from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import config from "./config";
+import { prisma } from "./lib/prisma";
 
 const app: Application = express();
 
@@ -16,8 +17,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get("/", (req: Request, res: Response) => {
-  res.json({ message: "This is the root Routee" });
+app.get("/", async (req: Request, res: Response) => {
+  const user = await prisma.user.findMany();
+
+  res.json({ data: user });
 });
 
 export default app;
