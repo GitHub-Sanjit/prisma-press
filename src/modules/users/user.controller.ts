998e17dcc3007470d7d1563838c55ca2eb28cad6
sanjit <sanjit.est14@gmail.com>
2026-06-29 12:17:21 +0600

    catchAsync Higher order function

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,12 +1,33 @@
-import { Request, Response } from "express";
-import { prisma } from "../../lib/prisma";
-import bcrypt from "bcryptjs";
-import config from "../../config";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status";
 import { userServices } from "./user.service";
+import { catchAsync } from "../../utils/catchAsync";
 
-const registerUser = async (req: Request, res: Response) => {
-  try {
+// const registerUser = async (req: Request, res: Response) => {
+//   try {
+//     const payload = req.body;
+
+//     const user = await userServices.userRegisterIntoDB(payload);
+
+//     res.status(httpStatus.CREATED).json({
+//       success: true,
+//       statusCode: httpStatus.CREATED,
+//       message: "User Register Successfully",
+//       data: { user },
+//     });
+//   } catch (error) {
+//     console.log(error);
+//     res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
+//       success: false,
+//       statusCode: httpStatus.INTERNAL_SERVER_ERROR,
+//       message: "Failed to Register",
+//       error: (error as Error).message,
+//     });
+//   }
+// };
+
+const registerUser = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
     const payload = req.body;
 
     const user = await userServices.userRegisterIntoDB(payload);
@@ -17,17 +38,8 @@ const registerUser = async (req: Request, res: Response) => {
       message: "User Register Successfully",
       data: { user },
     });
-  } catch (error) {
-    console.log(error);
-
-    res.status(httpStatus.INTERNAL_SERVER_ERROR).json({
-      success: false,
-      statusCode: httpStatus.INTERNAL_SERVER_ERROR,
-      message: "Failed to Register",
-      error: (error as Error).message,
-    });
-  }
-};
+  },
+);
 
 export const userController = {
   registerUser,
